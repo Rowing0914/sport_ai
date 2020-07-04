2915a5974942fabfea919c8e9af5841abe3e39bf
Rowing0914 <kosakaboat@gmail.com>
2020-07-04 17:55:19 +0900

    Added changes

--- src/param.py
+++ src/param.py
@@ -4,10 +4,10 @@ from src import ROOT_DIR
 class PARAMS(object):
     # for video_combine.py
     W, H = 960, 540
-    RIGHT_VIEW_PATH = ROOT_DIR + '/temp_video/right.mp4'
-    LEFT_VIEW_PATH = ROOT_DIR + '/temp_video/left.mp4'
-    OUTPUT_VIDEO = ROOT_DIR + '/temp_video/output.avi'
+    RIGHT_VIEW_PATH = ROOT_DIR + '/videos/right.mp4'
+    LEFT_VIEW_PATH = ROOT_DIR + '/videos/left.mp4'
+    OUTPUT_VIDEO = ROOT_DIR + '/videos/output.avi'
 
     # for app.py
     ALLOWED_EXTENSIONS = {"mp4"}
-    UPLOAD_FOLDER = './temp_video'
+    UPLOAD_FOLDER = './videos'
